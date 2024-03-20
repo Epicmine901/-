425cf260c83e997b9247e6435162220540191dea
Epicmine901 <148995702+Epicmine901@users.noreply.github.com>
2024-03-20 21:56:31 +0500

    C.cpp

--- C.cpp
+++ C.cpp
@@ -1,23 +1,29 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
+
 int main() {
-	int N, wins = 0, win_ = 0;
+	long  N, wins = 0;
+	long RSP[6] = {};
 	string koloda_1, koloda_2;
 	cin >> N;
-	if (1 <= N and N <= 50) {
+	if (1 <= N and N <= 100000) {
 		cin >> koloda_1 >> koloda_2;
-
 		for (int i = 0; i < N; i++) {
-			for (int win = 0; win < N; win++) {
-				if ((koloda_1[i] == 'R') and (koloda_2[win] == 'S')) { win_ = 1; koloda_2[win] = '0'; break; }
-				else if ((koloda_1[i] == 'S') and (koloda_2[win] == 'P')) { win_ = 1; koloda_2[win] = '0'; break; }
-				else if ((koloda_1[i] == 'P') and (koloda_2[win] == 'R')) { win_ = 1; koloda_2[win] = '0'; break; }
-				else win_ = 0;
-			}
-			wins = wins + win_;
-			win_ = 0;
+			if (koloda_1[i] == 'R')RSP[0]++;
+			if (koloda_2[i] == 'S')RSP[4]++;
+
+			if (koloda_1[i] == 'S')RSP[1]++;
+			if (koloda_2[i] == 'P')RSP[5]++;
+
+			if (koloda_1[i] == 'P')RSP[2]++;
+			if (koloda_2[i] == 'R')RSP[3]++;		
 		}
-		cout << wins;
+		wins = wins + min(RSP[0], RSP[4]);
+		wins = wins + min(RSP[1], RSP[5]);
+		wins = wins + min(RSP[2], RSP[3]);
+		cout << wins <<endl;
 	}
 	else cout << 0;
+	system("pause");
 }
